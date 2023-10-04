3efcdf58358b6d5dd6def780dfe20c8491bb0b41
nivregev <nivregev2@gmail.com>
2023-10-04 11:28:24 +0300

    frontend

---
+++ fronend/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import Navbar from "scenes/navbar";
+import ProfilePage from "scenes/profilePage";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/home" element={<HomePage />}></Route>
+          <Route path="/profile/:userId" element={<ProfilePage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
